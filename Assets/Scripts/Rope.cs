cce6f9b5ebbdb9f2e36da26cc0ab8df3f4b86ead
Razeltif <razeltif@gmail.com>
2019-03-22 13:57:48 +0100

    correction bug controleurs

---
+++ Assets/Scripts/Rope.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+
+    public Rigidbody hook;
+
+    public GameObject linkPrefab;
+
+    public int links = 7;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateRope();
+    }
+
+
+
+    void GenerateRope()
+    {
+
+        Rigidbody previousRB = hook;
+
+        for (int i = 0; i < links; i++)
+        {
+            GameObject link = Instantiate(linkPrefab, transform);
+            HingeJoint joint = link.GetComponent<HingeJoint>();
+            joint.connectedBody = hook;
+
+            previousRB = link.GetComponent<Rigidbody>();
+        }
+    }
+
+}
