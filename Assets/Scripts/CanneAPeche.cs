a0fc667c8e7677a2076afe2708fdefa3f1feac62
Razeltif <razeltif@gmail.com>
2019-03-29 20:20:38 +0100

    ajout possibilité de déplacer via script la pos de main sur canapeche

--- Assets/Scripts/CanneAPeche.cs
+++ Assets/Scripts/CanneAPeche.cs
@@ -8,6 +8,15 @@ public class CanneAPeche : GrablableObject
 
     private SteamVR_Behaviour_Pose handHoldingThis;
 
+    [SerializeField]
+    private float FirstHandPosition;
+    [SerializeField]
+    private float SecondHandPosition;
+
+
+
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,14 +29,14 @@ public class CanneAPeche : GrablableObject
     {
         if (isGrabbed)
         {
-            
-
+            Vector3 p;
+            p = this.transform.position + this.transform.up * FirstHandPosition;
 
             //Debug.Log("rotation de la main  : " + handHoldingThis.transform.rotation + "\nrotation de la canne à peche : " + this.transform.rotation);
 
             this.transform.rotation = handHoldingThis.transform.rotation;
             this.transform.Rotate(new Vector3(-90, 0, 0));
-            this.transform.position = handHoldingThis.transform.position;
+            this.transform.position = handHoldingThis.transform.position - this.transform.up * FirstHandPosition;
 
 
 
@@ -36,9 +45,10 @@ public class CanneAPeche : GrablableObject
 
     public override void Grab(SteamVR_Behaviour_Pose pose)
     {
-        isGrabbed = true;
 
         handHoldingThis = pose;
+
+        isGrabbed = true;
 
         this.GetComponent<Rigidbody>().isKinematic = true;
         this.GetComponent<Collider>().enabled = false;
@@ -55,6 +65,22 @@ public class CanneAPeche : GrablableObject
         this.GetComponent<Rigidbody>().velocity = handHoldingThis.GetVelocity();
         this.GetComponent<Rigidbody>().angularVelocity = handHoldingThis.GetAngularVelocity();
         handHoldingThis = null;
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Vector3 p;
+
+        // position de la  premiere main sur l'éditor
+        Gizmos.color = Color.yellow;
+        p = this.transform.position + this.transform.up * FirstHandPosition;
+        Gizmos.DrawSphere(p, 0.1f);
+        
+        // position de la  deuxième main sur l'éditor
+        Gizmos.color = Color.red;
+        p = this.transform.position + this.transform.up * SecondHandPosition;
+        Gizmos.DrawSphere(p, 0.1f);
     }
 
 }
