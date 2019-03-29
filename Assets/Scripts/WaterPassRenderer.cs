84fc69fd275cc2e15ebcbaf751852b1538ffe23d
VAIN Florent <Florent.VAIN@formation.enjmin.fr>
2019-03-29 19:58:11 +0100

    Ajout du premier jet du systeme d'eau vr

---
+++ Assets/Scripts/WaterPassRenderer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPassRenderer : MonoBehaviour
+{
+
+    public Camera renderCamera;
+    public RenderTexture renderTexture;
+    private RenderTexture normalTexture;
+    public Shader fastShaderReplacement;
+    public Shader normalShaderReplacement;
+
+    public GameObject waterPlane;
+
+    public int framCount = 60;
+    private int lastFrameCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        normalTexture = new RenderTexture(renderCamera.pixelWidth, renderCamera.pixelHeight, 24);
+        Shader.SetGlobalTexture("_CameraNormalsTexture", normalTexture);
+
+        //renderCamera.targetTexture = renderTexture;
+        //renderCamera.SetReplacementShader(fastShaderReplacement, "Opaque");
+
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        RenderTerrainNormals();
+        RenderWaterPrePass();
+    }
+
+    private void RenderWaterPrePass()
+    {
+        renderCamera.targetTexture = renderTexture;
+        //renderCamera.SetReplacementShader(fastShaderReplacement, "RenderType");
+
+        waterPlane.GetComponent<MeshRenderer>().material.SetTexture("_MainTex", renderTexture);
+
+        renderCamera.Render();
+    }
+
+    private void RenderTerrainNormals()
+    {
+        renderCamera.targetTexture = normalTexture;
+        renderCamera.RenderWithShader(normalShaderReplacement, "RenderType");
+    }
+}
---
+++ Assets/Shaders/HiddenNormalsTexture.shader
@@ -0,0 +1,59 @@
+﻿Shader "__XR__/Normals Texture"
+{
+    Properties
+    {
+    }
+    SubShader
+    {
+        Tags 
+		{ 
+			"RenderType" = "Opaque" 
+		}
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+				float3 normal : NORMAL;
+				
+				UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+				float3 viewNormal : NORMAL;
+
+				UNITY_VERTEX_OUTPUT_STEREO  //uint stereoEyeIndex : SV_RenderTargetIndex;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+				
+				UNITY_SETUP_INSTANCE_ID(v);
+				UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(0);
+
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.viewNormal = COMPUTE_VIEW_NORMAL;
+                return o;
+            }
+
+            float4 frag (v2f i) : SV_Target
+            {
+                return float4(i.viewNormal, 0);
+            }
+            ENDCG
+        }
+    }
+}
