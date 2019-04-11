4145ecd175bc7dc7494b2d1046c40f3f1db0afec
VAIN Florent <Florent.VAIN@formation.enjmin.fr>
2019-04-11 18:31:40 +0200

    Water scene

--- Assets/Scripts/WaterPassRenderer.cs
+++ Assets/Scripts/WaterPassRenderer.cs
@@ -29,7 +29,7 @@ public class WaterPassRenderer : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         RenderTerrainNormals();
         RenderWaterPrePass();
