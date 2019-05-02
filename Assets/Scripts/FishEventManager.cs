1f313f26a46e7d1e841f01e28d71e70f6e349272
Guillaume Dartoen <guillaume.dartoen@gmail.com>
2019-05-02 20:29:16 +0200

    Wildlife Manager, few animations & Fishrod settings

---
+++ Assets/Scripts/FishEventManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishEventManager : MonoBehaviour
+{
+    [SerializeField] ParticleSystem psJump;
+    [SerializeField] ParticleSystem psDive;
+    [SerializeField] GameObject koi;
+    [SerializeField] float particlesYOffset;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        psJump.Stop();
+        psDive.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SplashIn()
+    {
+        psJump.transform.position = new Vector3(koi.transform.position.x, koi.transform.position.y + particlesYOffset, koi.transform.position.z);
+        psJump.Play();
+    }
+
+    public void SplashOut()
+    {
+        psDive.transform.position = new Vector3(koi.transform.position.x, koi.transform.position.y + particlesYOffset, koi.transform.position.z);
+        Debug.Log(particlesYOffset);
+        Debug.Log(koi.transform.position.y + particlesYOffset);
+        psDive.Play();
+    }
+
+    public void DestroyFish()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/WildlifeManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WildlifeManager : MonoBehaviour
+{
+    float chanceJump, tirage;
+    float xMin, xMax;
+    float zMin, zMax;
+    float rotationRand;
+
+    bool fishExist;
+
+    [SerializeField] GameObject fishPrefab;
+    [SerializeField] GameObject waterPlane;
+    GameObject newFish;
+
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = fishPrefab.GetComponent<Animator>();
+        chanceJump = 12000;
+        xMin = waterPlane.transform.position.x;
+        xMax = waterPlane.transform.position.x + 10;
+        zMin = waterPlane.transform.position.z - 10;
+        zMax = waterPlane.transform.position.z + 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tirage = Random.Range(0, chanceJump);
+        if (tirage <= 1)
+        {
+            newFish = Instantiate(fishPrefab, transform);
+            newFish.transform.rotation = Quaternion.Euler(newFish.transform.rotation.x, Random.Range(-180, 180), newFish.transform.rotation.z);
+            newFish.transform.position = new Vector3(Random.Range(xMin, xMax), waterPlane.transform.position.y - 0.5f, Random.Range(zMin, zMax));
+        }
+     }
+}
