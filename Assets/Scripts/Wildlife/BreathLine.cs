1c2e52b534bbdee9db466a14557ed325ae31bc89
Guillaume Dartoen <guillaume.dartoen@gmail.com>
2019-05-17 14:03:08 +0200

    Background

--- Assets/Scripts/Wildlife/BreathLine.cs
+++ Assets/Scripts/Wildlife/BreathLine.cs
@@ -11,14 +11,20 @@ public class BreathLine : MonoBehaviour
     GameObject breathLineInst;
 
     bool lineSpawned;
-    [SerializeField] float maxCircleRadius;
+    float maxCircleRadius;
+    [SerializeField] float sizeMult;
     public AnimationCurve controleSpeed, controleSpeedOut;
     float lineSpeed;
     [SerializeField] float lineMult;
+    [SerializeField] float maxSizeDistance;
 
     Color currColor;
     SpriteRenderer circleSRenderer;
     bool breathingIn;
+
+    float distance;
+
+    Vector3 berge;
 
     void Start()
     {
@@ -28,11 +34,22 @@ public class BreathLine : MonoBehaviour
 
     void Update()
     {
-        if(bobber.isInWater() && !lineSpawned)
+
+        if (bobber.isInWater() && !lineSpawned)
         {
+            // get distance between the bobber and the berge 
+            berge = new Vector3(bobber.transform.position.x, bobber.transform.position.y, waterPlane.transform.position.z - waterPlane.transform.localScale.z * 5);
+            distance = Vector3.Distance(bobber.transform.position, berge);
+
+            if(distance > maxSizeDistance)
+            {
+                distance = maxSizeDistance;
+            }
+
             breathLineInst = Instantiate(breathLinePref);
             breathLineInst.transform.position = new Vector3(transform.position.x, waterPlane.transform.position.y, transform.position.z);
             circleSRenderer = breathLineInst.GetComponent<SpriteRenderer>();
+            maxCircleRadius = distance * sizeMult;
             currColor = circleSRenderer.color;
             currColor.a = 1f;
             circleSRenderer.color = currColor;
@@ -43,14 +60,14 @@ public class BreathLine : MonoBehaviour
         {
             if (breathLineInst.transform.localScale.x < maxCircleRadius && breathingIn)
             {
-                lineSpeed = controleSpeed.Evaluate(breathLineInst.transform.localScale.x / maxCircleRadius) * lineMult;
+                lineSpeed = controleSpeed.Evaluate(breathLineInst.transform.localScale.x / maxCircleRadius) * (lineMult / 1000 * distance);
                 breathLineInst.transform.localScale += new Vector3(lineSpeed, lineSpeed, lineSpeed);
                 breathLineInst.transform.position = new Vector3(transform.position.x, waterPlane.transform.position.y, transform.position.z);
                 circleSRenderer.color = currColor;
             }
             else if (breathLineInst.transform.localScale.x >= 0 && !breathingIn)
             {
-                lineSpeed = controleSpeedOut.Evaluate(breathLineInst.transform.localScale.x / maxCircleRadius) * lineMult;
+                lineSpeed = controleSpeedOut.Evaluate(breathLineInst.transform.localScale.x / maxCircleRadius) * (lineMult / 1000 * distance);
                 breathLineInst.transform.localScale -= new Vector3(lineSpeed, lineSpeed, lineSpeed);
                 breathLineInst.transform.position = new Vector3(transform.position.x, waterPlane.transform.position.y, transform.position.z);
                 circleSRenderer.color = currColor;
