bea79c88f93aea027220702b4485af60b06580f4
Guillaume Dartoen <guillaume.dartoen@gmail.com>
2019-05-28 17:34:12 +0200

    Background & new fog

--- Assets/FastBrouillard.cs
+++ Assets/FastBrouillard.cs
@@ -4,41 +4,34 @@ using UnityEngine;
 
 public class FastBrouillard : MonoBehaviour
 {
-    public GameObject brouillard1, brouillard2, brouillard3;
+    public GameObject[] brouillardD;
+    public GameObject[] brouillardG;
+    private float[] speed;
 
-    public float speedGlobal = 10;
-    private float speed1 = 1;
-    private float speed2 = 1;
-    private float speed3 = 1;
-
-    private Vector3 brouillard1BasePos, brouillard2BasePos, brouillard3BasePos;
+    private void Awake()
+    {
+        speed = new float[brouillardD.Length];
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        brouillard1BasePos = brouillard1.transform.position;
-        brouillard2BasePos = brouillard2.transform.position;
-        brouillard3BasePos = brouillard3.transform.position;
-
-        speed3 = speedGlobal;
-        speed2 = speed3 / 2;
-        speed1 = speed2 / 2;
+        for (int i = 0; i < brouillardD.Length; i++)
+        {
+            speed[i] = Random.Range(0.2f, 2);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        brouillard1.transform.Translate(Vector3.right * speed1 * Time.deltaTime);
-        brouillard2.transform.Translate(Vector3.left * speed2 * Time.deltaTime);
-        brouillard3.transform.Translate(Vector3.right * speed3 * Time.deltaTime);
-
-        if (brouillard1.transform.position.x >= brouillard1BasePos.x + 25 || brouillard1.transform.position.x <= brouillard1BasePos.x - 25)
-            speed1 *= -1;
-
-        if (brouillard2.transform.position.x <= brouillard2BasePos.x - 15 || brouillard2.transform.position.x >= brouillard2BasePos.x + 15)
-            speed2 *= -1;
-
-        if (brouillard3.transform.position.x >= brouillard3BasePos.x + 10 || brouillard3.transform.position.x <= brouillard3BasePos.x + 10)
-            speed3 *= -1;
+        for (int i = 0; i < brouillardD.Length; i++)
+        {
+            brouillardD[i].transform.Rotate(0, 0, 3 * speed[i] * Time.deltaTime);
+        }
+        for (int i = 0; i < brouillardG.Length; i++)
+        {
+            brouillardG[i].transform.Rotate(0, 0, 3 * -speed[i] * Time.deltaTime);
+        }
     }
 }
--- Assets/Scripts/Wildlife/RandomOffsetAnim.cs
+++ Assets/Scripts/Wildlife/RandomOffsetAnim.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class RandomOffsetAnim : MonoBehaviour
 {
+    Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        Animator anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
         AnimatorStateInfo state = anim.GetCurrentAnimatorStateInfo(0);//could replace 0 by any other animation layer index
         anim.Play(state.fullPathHash, -1, Random.Range(0f, 1f));
     }
@@ -15,6 +17,6 @@ public class RandomOffsetAnim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        anim.speed = 1.9f;
     }
 }
